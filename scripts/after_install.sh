0106e3aaa498b0b0da641b8a84264ee8444a6440
Barak shuli <barakshuli@gmail.com>
2023-12-18 21:33:27 +0200

    Delete scripts/after_install.sh

--- scripts/after_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script assumes the Angular project uses the Angular CLI for building.
-# Build the Angular application and restart the service (adjust as needed).
-
-echo "Running AfterInstall script..."
-
-cd /var/www/CypressPOC
-npm run build
