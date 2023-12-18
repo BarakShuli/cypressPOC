f929bd5cd8b4d5d869b573fb7df0f37e9f1cca52
Barak shuli <barakshuli@gmail.com>
2023-12-18 21:33:06 +0200

    Delete scripts/validate_service.sh

--- scripts/validate_service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Validate that the service is running successfully
-
-# Example: Check if the service is responding to HTTP requests
-response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
-if [ $response -ne 200 ]; then
-  echo "Validation failed: Service did not respond with HTTP 200 OK"
-  exit 1
-fi
