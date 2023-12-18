57fe5838c8af3407f04351118c69a8ec9e4a93a8
Barak shuli <barakshuli@gmail.com>
2023-12-18 09:11:23 +0200

    adding appsec file100

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Add any tasks you need to perform after the application is started
+
+# Example: Run database migrations
+cd /var/www/html/myapp
+python manage.py migrate
---
+++ scripts/before_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Add any tasks you need to perform before installing the new version
+
+# Example: Stop the existing service
+sudo service myapp stop
