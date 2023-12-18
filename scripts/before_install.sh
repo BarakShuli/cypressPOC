ebbb0500e12e5a0df97aed39171aa41843b6877f
Barak shuli <barakshuli@gmail.com>
2023-12-18 22:01:52 +0200

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mkdir -p /home/ec2-user/app/keys
-cp /home/ec2-user/keys/* /home/ec2-user/app/keys/
-chown -R ec2-user:ec2-user /home/ec2-user/app/keys
+# scripts/before-install.sh
+
+# Stop the existing Angular application (adjust based on your setup)
+sudo service nginx stop
