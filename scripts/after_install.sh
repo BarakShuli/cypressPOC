3dd926c69c1ca978fe5b416fca51de4d987ad481
Barak shuli <barakshuli@gmail.com>
2023-12-18 17:52:15 +0200

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Add any tasks you need to perform after the application is started
 
-# Example: Run database migrations
-cd /var/www/html/myapp
-python manage.py migrate
+# This script assumes the Angular project uses the Angular CLI for building.
+# Build the Angular application and restart the service (adjust as needed).
+
+echo "Running AfterInstall script..."
+
+cd /var/www/CypressPOC
+npm run build
